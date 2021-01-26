c98e45e76640a3ada97d80807015a2aee2d22892
sujiqiang@cvte.com <sujiqiang@cvte.com>
2021-01-26 22:37:27 +0800

    spring-security

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # exercise-project
 学习项目
+
+# eureka-server
+提供了服务注册的web管理页面，为了安全，加入spring-security，访问时需要认证
---
+++ eureka-server/src/main/java/com/sjq/exercise/eureka/server/SpringBootSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.sjq.exercise.eureka.server;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configurable
+@EnableWebSecurity
+public class SpringBootSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf
+        http.csrf().disable();
+        // 支持httpBasic
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
